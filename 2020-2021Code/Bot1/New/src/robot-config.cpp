e6752d5c1224950d1df8c08ca05d71aaf59af48f
nikaash <nikaashnigam@gmail.com>
2021-04-16 13:45:04 -0700

    i fucking hate vexcode

--- 2020-2021Code/Bot1/New/src/robot-config.cpp
+++ 2020-2021Code/Bot1/New/src/robot-config.cpp
@@ -9,14 +9,14 @@ brain  Brain;
 //controller
 controller Controller = controller(primary);
 //motors
-motor FLWheel = motor(PORT6, ratio6_1, false);
-motor FRWheel = motor(PORT3, ratio6_1, true);
-motor BLWheel = motor(PORT13, ratio6_1, true);
+motor FLWheel = motor(PORT20, ratio6_1, false);
+motor FRWheel = motor(PORT17, ratio6_1, true);
+motor BLWheel = motor(PORT12, ratio6_1, true);
 motor BRWheel = motor(PORT19, ratio6_1, false);
-motor MLWheel = motor(PORT17, ratio6_1, true);
-motor MRWheel = motor(PORT5, ratio6_1, false);
-motor Intake = motor(PORT15, ratio6_1, true);
-motor Lift = motor(PORT11, ratio18_1, false);
+motor MLWheel = motor(PORT8, ratio6_1, true);
+motor MRWheel = motor(PORT6, ratio6_1, false);
+motor Intake = motor(PORT9, ratio6_1, true);
+motor Lift = motor(PORT1, ratio18_1, false);
 //sensors
 inertial Inertia = inertial(PORT1);
 bumper LiftLimit = bumper(Brain.ThreeWirePort.G);
